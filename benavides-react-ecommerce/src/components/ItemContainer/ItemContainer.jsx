1c7a97d9c550c1bb34b1a8c0114b30e49da2111c
lbenav90 <leandronbenavides@gmail.com>
2022-10-29 13:37:04 -0300

    Add counter

--- benavides-react-ecommerce/src/components/ItemContainer/ItemContainer.jsx
+++ benavides-react-ecommerce/src/components/ItemContainer/ItemContainer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { useState } from 'react'
 import './ItemContainer.css'
 
 const ItemContainer = ({ greeting }) => {
+  const [ count, setState] = useState(0)
+
+  const add = () => {
+    setState(count + 1);
+  }
+
   return (
     <div className='item-container'>
-        { greeting }
+        { /*greeting*/ }
+        <p>{count}</p>
+        <button onClick={add}> + </button>
     </div>
   )
 }
